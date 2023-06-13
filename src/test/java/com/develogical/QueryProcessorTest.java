73f2ca2b9ba5c939b3a1a352dc3d9322fe352945
rbhu <reuben.b.hughes@gmail.com>
2023-06-13 13:40:26 +0000

    Return our name

--- src/test/java/com/develogical/QueryProcessorTest.java
+++ src/test/java/com/develogical/QueryProcessorTest.java
@@ -19,4 +19,9 @@ public class QueryProcessorTest {
   public void knowsAboutShakespeare() throws Exception {
     assertThat(queryProcessor.process("Shakespeare"), containsString("playwright"));
   }
+
+  @Test
+  public void returnsWhatIsOurName() throws Exception {
+    assertThat(queryProcessor.process("what is your name?"), is("TeamBrasil"));
+  }
 }
